73a980bc066211496b8e0b12c650c8591300dd7e
amuweee <mengyu.bai@gmail.com>
2020-07-19 14:21:56 -0400

    init

---
+++ ch2.jl
@@ -0,0 +1,12 @@
+# Defining Variables
+
+book = "Julia by Example"
+pi = 3.14
+ANSWER = 42
+my_first_name = "mbai"
+
+
+# Constants (immutable vars)
+
+const firstmonth = "January"
+const mealoption = "vegetarian"
