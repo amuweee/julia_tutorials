22b20d4bd8f18e112c2ca14448c9a2fb1f66a1ed
amuweee <mengyu.bai@gmail.com>
2020-07-21 15:24:37 -0400

    addings

--- eda.jl
+++ eda.jl
@@ -1,5 +1,8 @@
 using RDatasets
 using Statistics
+using Gadfly
+using CSV
+using Feather
 
 
 iris = dataset("datasets", "iris")
@@ -36,3 +39,33 @@ end
 
 # get random values
 rand(iris[!, :SepalLength], 5)
+
+# convert DataFrame into matrix
+irisarr = convert(Array, iris[:,:])
+
+
+# Visualizing using Gadfly
+plot(iris, x=:SepalLength, y=:PetalLength, color=:Species)
+plot(iris, x=:Species, y=:PetalLength, Geom.boxplot)
+plot(iris, x=:PetalLength, color=:Species, Geom.histogram)
+plot(iris, x=:PetalWidth, color=:Species, Geom.histogram)
+plot(iris, x=:PetalWidth, y=:PetalLength, color=:Species)
+
+
+# saving and loading
+# df slices
+iris[1:3, [:PetalLength, :PetalWidth]]
+
+# select a df section by applying bool filters
+test_data = iris[rand(150) .<= 0.1, [:PetalLength, :PetalWidth, :Species]]
+# assigning a random values between 0 and 1, and get a condition
+# so that 10% of total is returned as True, thus sliced for our selection
+
+
+# Saving files
+CSV.write("iris_test_data.csv", test_data)
+td = CSV.read("iris_test_data.csv")
+
+# Save to feather format
+Feather.write("iris_test_data.feather", test_data)
+td = Feather.read("iris_test_data.feather")
